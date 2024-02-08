fb545a5954456f4456177b1b64607b34458d9fdf
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-08 13:08:25 +0000

    added css resets and tailwind custom properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        mainBlue: "var(--main-blue)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
+      backgroundImage: {},
     },
   },
   plugins: [],
