7373464eab349f16ab24ed462e003f680924579f
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-15 12:17:50 +0000

    added basic data fetching

--- src/app/components/AccountCard/AccountCard.jsx
+++ src/app/components/AccountCard/AccountCard.jsx
@@ -1,20 +1,42 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import Card from "../UI/Card";
 import AccountHeader from "./AccountHeader";
 import SocialsSection from "./SocialsSection";
 import StatsSection from "./StatsSection";
 
+async function getGithubUser(username) {
+    let res = await fetch(`https://api.github.com/users/${username}`);
+    if (!res.ok) {
+        res = await fetch("https://api.github.com/users/octocat");
+    } else {
+        return res.json();
+    }
+}
+
 export default function AccountCard({ githubUser }) {
-    console.log("from card: " + githubUser.username);
+    const [githubUserData, setGithubUserData] = useState();
+
+    useEffect(() => {
+        getGithubUser("octocat").then((data) => setGithubUserData(data));
+    }, []);
+
+    useEffect(() => {
+        getGithubUser(githubUser.username).then((data) =>
+            setGithubUserData(data),
+        );
+    }, [githubUser]);
 
     return (
         <Card>
             <div className="p-[2.4rem] sm:p-[4rem] lg:ml-[18rem] lg:p-[4.8rem]">
                 <div className="mb-[3.4rem]">
-                    <AccountHeader />z
+                    <AccountHeader />
                 </div>
                 <p className="mb-[2.4rem] text-[1.3rem] text-textMain sm:text-[1.5rem] md:mb-[3rem]">
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Donec odio. Quisque volutpat mattis eros.
+                    {githubUserData.bio}
                 </p>
                 <div className="mb-[2.4rem] md:mb-[3.7rem]">
                     <StatsSection />
