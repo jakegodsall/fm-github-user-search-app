ee0c76b6f06923bcbd2f3af92c77ff18a6cdf4f9
jakegodsall <jake.edward.godsall@gmail.com>
2024-03-04 08:12:49 +0000

    added validation message with transitions on SearchBar component

--- src/app/components/SearchBar/SearchBar.jsx
+++ src/app/components/SearchBar/SearchBar.jsx
@@ -22,7 +22,7 @@ export default function SearchBar({ githubUser, setGithubUser, userFound }) {
                 onSubmit={onSubmitHandler}
                 className="flex w-full items-center justify-between py-[0.7remd] pl-[1.3rem] pr-[0.7rem] sm:px-[1.8rem]"
             >
-                <div className="relative flex items-center gap-[0.9rem] sm:gap-[2.3rem]">
+                <div className="flex items-center gap-[0.9rem] sm:gap-[2.3rem]">
                     <Image
                         src={searchIcon}
                         width="20"
@@ -30,18 +30,6 @@ export default function SearchBar({ githubUser, setGithubUser, userFound }) {
                         alt="magnifying glass"
                         className="h-[2rem] w-[2rem]"
                     />
-                    <AnimatePresence>
-                        {!userFound && (
-                            <motion.p
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                className="absolute right-[-1rem] bg-foreground px-[1rem] py-[1.8rem] text-[1.2rem] text-[#f74646]"
-                            >
-                                No results
-                            </motion.p>
-                        )}
-                    </AnimatePresence>
                     <input
                         className="bg-inherit py-[1.8rem] text-[1.3rem] text-[#4B6A9B] placeholder:text-[#4B6A9B] focus:outline-none sm:w-[28rem] sm:text-[1.8rem]"
                         type="text"
@@ -49,7 +37,21 @@ export default function SearchBar({ githubUser, setGithubUser, userFound }) {
                         name="username"
                     />
                 </div>
-                <Button>Search</Button>
+                <div className="ml-auto flex items-center">
+                    <AnimatePresence>
+                        {!userFound && (
+                            <motion.p
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                className="hidden bg-foreground px-[1rem] py-[1.8rem] text-center text-[1.2rem] text-[#f74646] sm:block"
+                            >
+                                No results
+                            </motion.p>
+                        )}
+                    </AnimatePresence>
+                    <Button>Search</Button>
+                </div>
             </form>
         </Card>
     );
